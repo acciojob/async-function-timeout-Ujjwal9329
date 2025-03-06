8690bb388c5f41e855c621ad7fc9839fe13a399e
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-03-06 12:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",asyn function(){
+	const text = document.getElementById("text").value;
+	const delay = parseInt(document.getElementById("delay").value);
+	const output = document.getElementById("output").value;
+if(!text || isNaN(delay)|| delay<0){
+   output.innerText = "Please enter a valid text and delay."
+   return;
+}
+output.innerText = "Waiting...";
+
+await delayMessage(delay);
+
+output.innerText = text;
+
+
+});
+function delayMessage(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
